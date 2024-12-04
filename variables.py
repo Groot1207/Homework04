8cfa92d753bba682be3fdae1cf02ffca1f6b24c1
Groot1207 <sere8887@gmail.com>
2024-12-04 16:17:17 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+the_number_of_completed_dz = 12.0
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_lesson = (number_of_hours_spent / the_number_of_completed_dz)
+print(course_name,the_number_of_completed_dz,number_of_hours_spent,time_for_one_lesson)
